5b4a3285903fadd23765fac3fb4756a25d2b0123
Michal Kubista <kubistmi@gmail.com>
2021-06-27 18:34:13 +0000

    Extend  for OnRegOracle and EpsGreedPolicy - different param grid

--- train_models.py
+++ train_models.py
@@ -1,4 +1,3 @@
-from datetime import datetime
 import os
 import pickle
 
@@ -6,7 +5,10 @@ import mlflow as mf
 import numpy as np
 import pandas as pd
 
-from interface import  AdaGreedPolicy, Agent, BatchProvider, SampleProvider, ChurnEnvironment, OnRegOracle, SampleProvider, learn
+from interface import AdaGreedPolicy, EpsGreedPolicy
+from interface import BatchProvider, SampleProvider
+from interface import NeuralOracle, OnRegOracle, RegTreeOracle
+from interface import Agent, ChurnEnvironment, learn
 
 ITER = 8000
 
@@ -19,49 +21,54 @@ rewards = pd.DataFrame({
     "act":  [0,    0,    1,    1],
     "pred": [0,    1,    0,    1],
     "val":  [1, -1.5,   -2,  0.5]
-    #"val":  [0,  -50, -350,   30]
 })
 
+# parameters
 ada_thresh = np.array([0.8, 0.9, 0.99])
 ada_decay = np.array([0.9, 0.99, 0.999])
+eps_thresh = np.array([0.1, 0.2, 0.3])
+eps_decay = np.array([0])
 retrains = np.array([100, 200])
 
-# RegTree specific
-# sample_size = np.array([300, 500])
-# depths = np.array([3, 5, 7, 9])
-# prod = pd.core.reshape.util.cartesian_product([ada_decay, ada_thresh, depths, sample_size, retrains])
+# OnReg specific ###############################################################
+# prod = pd.core.reshape.util.cartesian_product([eps_thresh, eps_decay, retrains])
 # param_grid = pd.DataFrame({
 #     "thresh": prod[0],
 #     "decay": prod[1],
-#     "depth": prod[2],
-#     "size": prod[3],
-#     "retrain": prod[4]
+#     "retrain": prod[2]
 # })
+# param_grid = param_grid.assign(size = param_grid.retrain)
 
-prod = pd.core.reshape.util.cartesian_product([ada_decay, ada_thresh, retrains])
+# RegTree specific #############################################################
+sample_size = np.array([300, 500])
+depths = np.array([3, 5, 7, 9])
+prod = pd.core.reshape.util.cartesian_product([eps_thresh, eps_decay, depths, sample_size, retrains])
 param_grid = pd.DataFrame({
     "thresh": prod[0],
     "decay": prod[1],
-    "retrain": prod[2]
+    "depth": prod[2],
+    "size": prod[3],
+    "retrain": prod[4]
 })
-param_grid = param_grid.assign(size = param_grid.retrain)
+
+# Neural specific ##############################################################
 
 env = ChurnEnvironment(data, rewards)
 
 def evaluate(x):
     p = x.to_dict()
     with mf.start_run():
-        mf.log_param("oracle", "OnReg")
-        mf.log_param("policy", "AdaGreed")
-        mf.log_param("provider", "batch")
+        mf.log_param("oracle", "RegTree")
+        mf.log_param("policy", "EpsGreed")
+        mf.log_param("provider", "sample")
         mf.log_params(p)
-        agent = Agent(BatchProvider(int(p["size"])), OnRegOracle([0,1], -2), AdaGreedPolicy(thresh = p["thresh"], decay = p["decay"]))
+        agent = Agent(SampleProvider(int(p["size"])), RegTreeOracle([0,1], -2, p["depth"]), EpsGreedPolicy(thresh = p["thresh"]))#, decay = p["decay"]))
         agent = learn(agent, env, iters = ITER, update_freq = p["retrain"])
         rew = pd.Series(agent.provider.rewards)
         act = pd.Series(agent.provider.actions)
         #
-        # dirname = f"models/SGD_{p['depth']}_{p['size']}_{p['retrain']}_{p['thresh']}_{p['decay']}/"
-        dirname = f"models/SGD_{p['size']}_{p['retrain']}_{p['thresh']}_{p['decay']}/"
+        dirname = f"models/tree_Eps_{p['depth']}_{p['size']}_{p['retrain']}_{p['thresh']}_{p['decay']}/"
+        # dirname = f"models/SGD_Eps_{p['size']}_{p['retrain']}_{p['thresh']}_{p['decay']}/"
         os.makedirs(dirname)
         rew.to_csv(dirname + "rewards.csv", index = False)
         act.to_csv(dirname + "actions.csv", index = False)
